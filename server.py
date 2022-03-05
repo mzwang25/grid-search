c82173f47a39a2a5d7bfd3e67626550c48e15bf8
Michael Wang <mzwang25@michaels-air.lan>
2022-03-05 12:38:47 -0800

    submission backend

--- server.py
+++ server.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask( __name__ )
 
 @app.route( "/" )
 def index():
   return render_template( "index.html" )
+
+@app.route( "/submit" )
+def submit():
+  classname = request.args.get( "classname" )
+  time = request.args.get( "time" )
+  day = request.args.get( "day" )
+  
+  return render_template( "loading.html" )
 
 if __name__ == "__main__":
   app.run()
